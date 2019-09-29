2198be5cb6c288b0264c865f99d137a85b0ccdde
Andrej Leban <andrejleban@gmail.com>
2019-09-29 21:14:13 +0200

    Tools: print compiler version

---
+++ tools.cpp
@@ -0,0 +1,11 @@
+/** \file tools.cpp
+ * \author Andrej Leban
+ * \date 9/2019
+ */
+
+#include "tools.h"
+
+namespace cm
+{
+
+} // namespace cm
---
+++ tools.h
@@ -0,0 +1,26 @@
+/** \file tools.h
+ * \author Andrej Leban
+ * \date 9/2019
+ */
+
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <iostream>
+
+namespace cm
+{
+
+inline void compiler_version()
+{
+
+#ifdef __clang__
+    std::cout <<"Clang " << __clang_major__ << "." << __clang_minor__ << '\n';
+#else
+    std::cout <<"GCC " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << '\n';
+#endif
+
+}
+
+} // namespace cm
+#endif // TOOLS_H
