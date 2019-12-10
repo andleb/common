50d962dab287ebf1ad18805d35d0b572ee43fa0b
Andrej Leban <andrejleban@gmail.com>
2019-12-10 17:06:46 +0100

    Functional.h: added move_accumulate
    As suggested by I. Čukić in Functional programming in C++

---
+++ functional.h
@@ -0,0 +1,33 @@
+#ifndef FUNCTIONAL_H
+#define FUNCTIONAL_H
+
+#include <utility>
+
+namespace cm {
+
+// A modified implementation of std::accumulate which avoids
+// unnecessary copies – whenever it calls the folding function,
+// it moves the previously accumulated value into it instead
+// of passing a copy
+
+// Copyright I. Čukić, Functional programming in C++
+template <typename BeginIt, typename EndIt, typename T, typename F>
+T moving_accumulate(BeginIt first, const EndIt& last, T init,
+                    F folding_function)
+{
+    for (; first != last; ++first) {
+        // When passing the accumulated value to
+        // the folding function, move from it.
+        // This might make problems for types that
+        // do not support assignment to moved-from values,
+        // but it will work for most types without problems.
+        init = folding_function(std::move(init), *first);
+    }
+
+    return init;
+}
+
+
+} // namespace cm
+
+#endif // FUNCTIONAL_H
